e3829ceab475975d3ce20ffc177d35de928fb2e8
Cvetan65 <cvetan1965@gmial.com>
2021-06-02 02:02:39 +0200

    13

--- src/features/profiles/profileActions.js
+++ src/features/profiles/profileActions.js
@@ -1,4 +1,4 @@
-import { LISTEN_TO_CURRENT_USER_PROFILE, LISTEN_TO_SELECTED_USER_PROFILE } from './profileContstants';
+import { LISTEN_TO_CURRENT_USER_PROFILE, LISTEN_TO_SELECTED_USER_PROFILE, LISTEN_TO_USER_PHOOTOS } from './profileContstants';
 
 export function listenToCurrentUserProfile(profile) {
     return {
@@ -11,5 +11,12 @@ export function listenToSelectedUserProfile(profile) {
     return {
         type: LISTEN_TO_SELECTED_USER_PROFILE, 
         payload: profile
+    }
+}
+
+export function listenToUserPhotos(photos) {
+    return {
+        type: LISTEN_TO_USER_PHOOTOS, 
+        payload: photos
     }
 }
--- src/features/profiles/profilePage/ProfileContent.jsx
+++ src/features/profiles/profilePage/ProfileContent.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Tab } from 'semantic-ui-react'
 import AboutTab from './AboutTab'
+import PhotosTab from './PhotosTab'
 
 export default function ProfileContent({profile, isCurrentUser}) {
 const panes=[
     {menuItem: 'За',  render: ()=> <AboutTab profile={profile} isCurrentUser={isCurrentUser}/>},
-    {menuItem:'Фотографии ', render: ()=> <Tab.Pane>Photos</Tab.Pane>},
+    {menuItem:'Фотографии ', render: ()=> <PhotosTab profile={profile} isCurrentUser={isCurrentUser} />},
     {menuItem:'Понуди', render: ()=> <Tab.Pane>Events</Tab.Pane>},
     {menuItem:'Следачи', render: ()=> <Tab.Pane>Followers</Tab.Pane>},
     {menuItem:'Следи', render: ()=> <Tab.Pane>Following</Tab.Pane>},
@@ -16,6 +17,6 @@ const panes=[
         menu={{fluid: true, vertical: true}}
         menuPosition='right'
         panes={panes}
-        />
+                />
     )
 }
